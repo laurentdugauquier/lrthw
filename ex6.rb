f5919b02ae363a4fd6cd35def8538d4105275301
laurentdugauquier <laurent.dugauquier@sfr.fr>
2015-04-25 17:46:04 +0200

    fini les exercices 2 à 13

---
+++ ex6.rb
@@ -0,0 +1,19 @@
+types_o_people = 10
+x = "There are #{types_of_people} types_of_people."
+binary = "binary"
+do_not = "don't"
+y = "those who know"{binary} and those who #{do_not}."
+puts x
+puts y
+puts "I said: #{x}."
+puts "I also said: '#{y}."
+
+hilarious = false
+joke_evaluation = "Isn't that joke so funny?! #{hilarious}"
+
+puts joke_evaluation
+
+w = "this is the left side of..."
+e = "a string with a right side."
+
+puts w + e
